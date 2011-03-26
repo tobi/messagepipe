ae07ef21f302294479ec4afcc8d93a01ce9aa6e9
Tobias Lütke <tobi@jadedpixel.com>
2011-03-26 16:02:24 -0400

    initial import

---
+++ client.rb
@@ -0,0 +1,79 @@
+require 'rubygems'
+require 'eventmachine'    # EventMachine
+require 'msgpack'
+require 'json'
+
+class MessagePipeSocket
+  CMD_CALL = 0x01
+  RET_OK   = 0x02
+  RET_E    = 0x03
+
+  class RemoteError < StandardError
+  end
+
+  def initialize(host, port)
+    @socket = TCPSocket.new(host, port)
+    @unpacker = MessagePack::Unpacker.new
+  end
+
+  def call(method, *args) 
+    @socket.print([CMD_CALL, method, args].to_msgpack)
+
+    loop do 
+      @unpacker.feed(@socket.recv(4096))
+      @unpacker.each do |msg| 
+        case msg.first
+        when RET_E
+          raise RemoteError, msg[1]
+        when RET_OK
+          return msg[1]
+        else
+          raise RemoteError, "recieved invalid message: #{msg.inspect}"
+        end
+      end
+      raise RemoteError, 'disconnected' unless @socket.open? 
+    end
+  end
+end
+
+
+
+
+if __FILE__ == $0
+  require "test/unit"
+
+  class TestCase < Test::Unit::TestCase
+
+    def setup
+      $socket ||= MessagePipeSocket.new 'localhost', 9191
+    end
+
+    def test_simple_rpc
+      assert_equal 'hello', $socket.call(:hi)
+    end
+
+    def test_rpc_with_params
+      assert_equal 3, $socket.call(:add, 1, 2)
+      assert_equal 2000000, $socket.call(:add, 1000000, 1000000)
+    end
+
+    def test_throw_exception
+      assert_raise(MessagePipeSocket::RemoteError) do
+        $socket.call :throw
+      end    
+    end
+
+    def test_cannot_call_non_existing_method
+      assert_raise(MessagePipeSocket::RemoteError) do
+        $socket.call :does_not_exist
+      end    
+    end
+
+    def test_cannot_call_private_method
+      assert_raise(MessagePipeSocket::RemoteError) do
+        $socket.call :private_method
+      end    
+    end 
+  end
+
+end
---
+++ server.rb
@@ -0,0 +1,74 @@
+require 'rubygems'
+require 'eventmachine'    # EventMachine
+require 'msgpack'
+require 'json'
+
+# Server that receives MessagePack and returns JSON
+class MessagePipeServer < EventMachine::Connection
+  CMD_CALL = 0x01
+  RET_OK   = 0x02
+  RET_E    = 0x03
+
+  protected 
+
+  def pac
+    @pac ||= MessagePack::Unpacker.new  # Stream Deserializer
+  end
+
+  def receive_data(data)    
+    pac.feed(data)        
+    pac.each do |msg|      
+      begin        
+        result = receive_object(msg)        
+        send_data([RET_OK, result].to_msgpack)
+      rescue => e
+        send_data([RET_E, "#{e.class.name}: #{e.message}"].to_msgpack)
+      end
+    end
+  end
+
+  def receive_object(msg)    
+    cmd, method, args = *msg
+
+    puts "* #{method} with #{args.length} arg(s)"
+
+
+    if cmd != CMD_CALL
+      close
+      raise 'Bad client'
+    end
+
+    if method and public_methods.include?(method)
+      __send__(method, *args)
+    else
+      raise NoMethodError, "no method #{method} found."
+    end
+  end
+end
+
+class TestServer < MessagePipeServer
+
+  def add(a, b)
+    a + b
+  end
+
+  def hi
+    'hello'
+  end
+
+  def throw
+    raise StandardError, 'hell'
+  end
+
+  private
+
+  def private_method
+    'oh no'
+  end
+
+end
+
+EventMachine::run do
+  EventMachine::start_server "0.0.0.0", 9191, TestServer
+end
+
